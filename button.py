72ce16338806c58a7a1ed44693da37b072fd19a2
Ytyt990 <95852520+YutoYuto990@users.noreply.github.com>
2022-05-12 11:27:11 +0900

    Update button.py

--- button.py
+++ button.py
@@ -21,13 +21,38 @@ class Pager:
     self.timeout=timeout
     
   async def start(self,interaction):
-    if isinstance(interaction,discord.Interaction):
-      msg=await interaction.response.send_message(embed=discord.Embed(description=f"{self.prefix}{self.embed[0]}{self.suffix}"),view=PageButton(emb=self.embed,author=self.author,prefix=self.prefix,suffix=self.suffix,timeout=self.timeiut))
-    if isinstance(interaction,discord.ext.commands.Context):
-      msg=await interaction.send(embed=discord.Embed(description=f"{self.prefix}{self.embed[0]}{self.suffix}"),view=PageButton(emb=self.embed,author=self.author,prefix=self.prefix,suffix=self.suffix,timeout=self.timeout))
-    if self.timeout:
-      await asyncio.sleep(self.timeout)
-      await msg.edit(view=None)
+import discord
+import asyncio
+class PageButton(discord.ui.View):
+    def __init__(self,emb,author=None,styles=[discord.ButtonStyle.blurple,discord.ButtonStyle.blurple,discord.ButtonStyle.danger,discord.ButtonStyle.blurple,discord.ButtonStyle.blurple],args=["⏪","◀️","⏹","▶️","⏩"],ids=["first","back","stop","next","end"],prefix="",suffix="",timeout=180):
+        super().__init__()
+        if len(emb)>1:
+          for style,txt,id in zip(styles,args,ids):
+            self.add_item(HugaButton(emb,author,txt,style,id,prefix,suffix,timeout))
+        if len(emb)==1:
+          self.add_item(HugaButton(emb,author,args[2],styles[2],ids[2],prefix,suffix,timeout))
+        self.emb=emb
+        self.args=args
+        self.ids=ids
+
+class Pager:
+  def __init__(self,embed,timeout,prefix="",suffix="",author=False):
+    self.embed=embed
+    self.prefix=prefix
+    self.suffix=suffix
+    self.author=author
+    self.timeout=timeout
+    
+  async def start(self,interaction):
+      if isinstance(interaction,discord.Interaction):
+          msg=await interaction.response.send_message(embed=discord.Embed(description=f"{self.prefix}{self.embed[0]}{self.suffix}"),view=PageButton(emb=self.embed,author=self.author,prefix=self.prefix,suffix=self.suffix,timeout=self.timeout))
+          
+      if isinstance(interaction,discord.ext.commands.Context):
+        msg=await interaction.send(embed=discord.Embed(description=f"{self.prefix}{self.embed[0]}{self.suffix}"),view=PageButton(emb=self.embed,author=self.author,prefix=self.prefix,suffix=self.suffix,timeout=self.timeout))
+      if self.timeout:
+        await asyncio.sleep(self.timeout)
+        await msg.edit(view=None)
+      return msg.author
 n=0
 class HugaButton(discord.ui.Button):
     def __init__(self,emb,author,txt,style,id,prefix,suffix,timeout):
@@ -37,10 +62,15 @@ class HugaButton(discord.ui.Button):
         self.author=author
         self.timeout=timeout
         super().__init__(label=txt,style=style,custom_id=id)
+    async def callback(self,interaction:discord.Interaction):
+      if self.author:
+        if interaction.user==self.author:
+          await self.call(interaction)
+      else:
+        await self.call(interaction)
 
-    async def callback(self,interaction):
-      global n
-      if self.author==interaction.user:
+    async def call(self,interaction):
+        global n
         if self.custom_id=="next":
           if n<=len(self.emb):
             await interaction.response.edit_message(embed=discord.Embed(description=f"{self.prefix}{self.emb[n+1]}{self.suffix}"))
